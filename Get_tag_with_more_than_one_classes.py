e3fa6c667e32c1f3e99aa5ec5dd4b4ff3dfd86ec
inasalkamachy <56752381+inasalkamachy@users.noreply.github.com>
2021-06-17 12:52:27 +0300

    Update Get_tag_with_more_than_one_classes.py

--- Get_tag_with_more_than_one_classes.py
+++ Get_tag_with_more_than_one_classes.py
@@ -6,6 +6,8 @@ result = requests.get(user_url).text
 soup = BeautifulSoup(result, 'html.parser')
 #find = soup.find("h2")
 #print(find.text)
+#soup.find_all('', {'class':'member_info'})
+
 
 find_class = soup.find_all('div', {'class':{'member_info', 'col-lg-5 col-md-12 col-sm-12'}})
 for class_ in find_class:
